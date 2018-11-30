2f171a3372b6e55c17694f5a197cb5f920d403a8
lucasrochademoraes <36646269+lucasrochademoraes@users.noreply.github.com>
2018-11-30 17:23:23 -0200

    Create DropdownExtension.cs
    Generic dropdown itens

---
+++ DropdownExtension.cs
@@ -0,0 +1,39 @@
+namespace AutoFinderDETRAN.Domain.Extensions
+{
+    public static class DropdownExtension
+    {
+        public static List<GenericDropdownDTO> ToDropdown<T>(this List<T> list, string text, string value)
+        {
+            List<GenericDropdownDTO> dropdown = new List<GenericDropdownDTO>();
+
+            foreach (var item in list)
+            {
+                var sItem = new GenericDropdownDTO();
+                sItem.Text = item.GetType().GetProperty(text).GetValue(item, null) as string;
+                sItem.Value = item.GetType().GetProperty(value).GetValue(item, null) as string;
+
+                dropdown.Add(sItem);
+
+            }
+
+            return dropdown;
+        }
+    }
+    
+    public class GenericDropdownDTO
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+
+        public GenericDropdownDTO()
+        {
+            
+        }
+
+        public GenericDropdownDTO(string value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+    }
+}
