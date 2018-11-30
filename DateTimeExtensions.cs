7803ef405efd50fe08aee4a4bcca73b9f8635610
lucasrochademoraes <36646269+lucasrochademoraes@users.noreply.github.com>
2018-11-30 17:21:40 -0200

    Update DateTimeExtensions.cs

--- DateTimeExtensions.cs
+++ DateTimeExtensions.cs
@@ -26,7 +26,8 @@ namespace SuricataModeler.Build.Domain.Extensions
 
         public static int DiffDays(this DateTime data, DateTime dataParametro)
         {
-            return (DateTime.Compare(data, dataParametro));
+            var date = (DateTime.Compare(data, dataParametro));
+            return date;
         }
 
         public static int DiffDays(this DateTime data, DateTime? dataParametro)
@@ -44,12 +45,43 @@ namespace SuricataModeler.Build.Domain.Extensions
 
         public static bool IsDateBetween(this DateTime dataParametro , DateTime dataIni, DateTime dataFim)
         {
-            return (dataParametro >= dataIni && dataParametro <= dataFim);
+            return (dataParametro.Date >= dataIni.Date && dataParametro.Date <= dataFim.Date);
         }
 
         public static bool IsTimeBetween(this TimeSpan horaParametro, TimeSpan horaIni, TimeSpan horaFim)
         {
             return (horaParametro >= horaIni && horaParametro <= horaFim);
+        }
+
+        public static DateTime FirstDayOfMonth(this DateTime data)
+        {
+            return new DateTime(data.Year,data.Month,1);
+        }
+
+        public static DateTime ToDateTime(this string data)
+        {
+            return !string.IsNullOrEmpty(data) ? DateTime.ParseExact(data, "dd/MM/yyyy", CultureInfo.InvariantCulture) : DateTime.MinValue;
+        }
+
+        public static DateTime ToDateTimehhmm(this string data)
+        {
+            return !string.IsNullOrEmpty(data) ? DateTime.ParseExact(data, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture) : DateTime.MinValue;
+        }
+
+        public static DateTime? ToNullableDateTime(this string data)
+        {
+            return !string.IsNullOrEmpty(data) ? DateTime.ParseExact(data, "dd/MM/yyyy", CultureInfo.InvariantCulture) : (DateTime?)null;
+        }
+
+        public static DateTime LastDayOfMonth(this DateTime data)
+        {
+            var first = new DateTime(data.Year, data.Month, 1);
+            return first.AddMonths(1).AddDays(-1);
+        }
+
+        public static TimeSpan IncrementMinutes(this TimeSpan time, int value)
+        {
+            return TimeSpan.FromMinutes(time.TotalMinutes + value);
         }
     }
 }
